8f978f95ae99520560cb2b7a60667391403ac3bc
Evanyl <55635347+Evanyl@users.noreply.github.com>
2020-07-12 21:50:13 -0700

    Update levels.js

--- src/levels.js
+++ src/levels.js
@@ -28,9 +28,9 @@ export default class LevelHandler {
 		for (let rowIndex = 0; rowIndex < 20; rowIndex++) {
 			let row = [];
 			for (let columnIndex = 0; columnIndex < 10; columnIndex++) {
-				if (rowIndex >= 15) {
+				if (rowIndex >= 17) {
 					row.push(0);
-				} else if (Math.random() < 0.4) {
+				} else if (Math.random() < 0.5) {
 					row.push(1);
 				} else {
 					row.push(0);
