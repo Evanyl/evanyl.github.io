3cb37accfb49739e6609c98fb67eb2b79a8a962b
Evan Liu <liuyievan@gmail.com>
2020-07-21 16:24:47 -0700

    Added a procfile

--- chat/index.js
+++ chat/index.js
@@ -2,6 +2,12 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
@@ -17,6 +23,6 @@ io.on('connection', (socket) => {
 	});
 });
 
-http.listen(3000, () => {
-	console.log('listening on *:3000');
+http.listen(port, () => {
+	console.log(`listening on *:${port}`);
 });
